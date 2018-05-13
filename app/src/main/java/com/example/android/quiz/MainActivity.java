1f350706952d4d52c05047e6d700d9dcaf326f09
Michael G Rios <michael.rios093@gmail.com>
2018-05-13 19:43:39 -0400

    shows correct or wrong

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     int question1 = 0;
     int question2 = 0;
-    int score =0;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,67 +34,79 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
 
-        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whippedCream);
-        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate);
+        CheckBox oneLolliPop = (CheckBox) findViewById(R.id.whippedCream);
+        CheckBox twoLolliPop = (CheckBox) findViewById(R.id.chocolate);
         //Log.v("MainActivity", cName );
 
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        boolean hasChocolate = chocolateCheckBox.isChecked();
+        boolean hasOneLolliPop = oneLolliPop.isChecked();
+        boolean hasTwoLolliPop = twoLolliPop.isChecked();
 
         score = question1 + question2;
-        if (hasWhippedCream){
+        if (hasOneLolliPop && hasTwoLolliPop) {
             score++;
         }
 
-        String message = createOrderSummary(hasWhippedCream, hasChocolate, score);
+        String message = createOrderSummary(hasOneLolliPop, hasTwoLolliPop, score);
+        TextView questionOneTextView = (TextView) findViewById(R.id.answer_radio_question_1);
+        TextView questionTwoTextView = (TextView) findViewById(R.id.answer_radio_question_2);
+        TextView questionOneCheckTextView = (TextView) findViewById(R.id.answer_check_question_1);
 
+        displayMessageCheckBox(hasOneLolliPop, hasTwoLolliPop, questionOneCheckTextView);
+        displayMessageRadioQuestion2(question1, questionOneTextView);
+        displayMessageRadioQuestion2(question2, questionTwoTextView);
         displayMessage(message);
-        }
+    }
 
 
-        public void onRadioButtonClicked(View view) {
-            // Is the button now checked?
-            boolean checked = ((android.widget.RadioButton) view).isChecked();
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((android.widget.RadioButton) view).isChecked();
 
-            // Check which radio button was clicked
-            switch(view.getId()) {
-                case R.id.radio_one:
-                    if (checked)
-                        question1 = 1;// Pirates are the best
-                    break;
-                case R.id.radio_two:
-                    if (checked)
-                        question1 = 0;// Ninjas rule
-                    {break;}
-                    case R.id.radio_three:
-                    if (checked)
-                        question1 = 0;// Pirates are the best
-                    break;
-                case R.id.radio_four:
-                    if (checked)
-                        question1 = 0;// Ninjas rule
-                    {break;}
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radio_one:
+                if (checked)
+                    question1 = 1;// Pirates are the best
+                break;
+            case R.id.radio_two:
+                if (checked)
+                    question1 = 0;// Ninjas rule
+            {
+                break;
             }
-
-            switch(view.getId()) {
-                case R.id.radio2_one:
-                    if (checked)
-                        question2 = 0;// Pirates are the best
-                    break;
-                case R.id.radio2_two:
-                    if (checked)
-                        question2 = 1;// Ninjas rule
-                    break;
-                case R.id.radio_three:
-                    if (checked)
-                        question2 = 0;// Pirates are the best
-                    break;
-                case R.id.radio_four:
-                    if (checked)
-                        question2 = 0;// Ninjas rule
-                    {break;}
+            case R.id.radio_three:
+                if (checked)
+                    question1 = 0;// Pirates are the best
+                break;
+            case R.id.radio_four:
+                if (checked)
+                    question1 = 0;// Ninjas rule
+            {
+                break;
             }
         }
+
+        switch (view.getId()) {
+            case R.id.radio2_one:
+                if (checked)
+                    question2 = 0;// Pirates are the best
+                break;
+            case R.id.radio2_two:
+                if (checked)
+                    question2 = 1;// Ninjas rule
+                break;
+            case R.id.radio2_three:
+                if (checked)
+                    question2 = 0;// Pirates are the best
+                break;
+            case R.id.radio2_four:
+                if (checked)
+                    question2 = 0;// Ninjas rule
+            {
+                break;
+            }
+        }
+    }
 
     /**
      * This method displays the given quantity value on the screen.
@@ -110,11 +122,31 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-        private void displayMessage(String message) {
+    private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.answer_text_view);
         orderSummaryTextView.setText(message);
     }
 
+
+    private void displayMessageRadioQuestion2(int isCorrect, TextView answer) {
+
+        if (isCorrect == 1) {
+        answer.setText("correct");
+        }
+        else {
+            answer.setText("wrong");
+        }
+    }
+
+    private void displayMessageCheckBox(Boolean A, Boolean B, TextView answer) {
+
+        if (A && B) {
+            answer.setText("correct");
+        }
+        else {
+            answer.setText("wrong");
+        }
+    }
 
 
 }
