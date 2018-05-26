143a53f9359916b4a5449837ce28b187c888a499
Michael Rios <michael.rios093@gmail.com>
2018-05-26 17:19:13 -0400

    real but non-senscical questions added, code cleaned up a bit

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -19,9 +19,17 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    //initializing question values for radio questions, default is false
     int question1 = 0;
     int question2 = 0;
     int question3 = 0;
+
+    //for radio questions lets user know which is the right answer
+    String wrongQuestionOne = "One is the correct answer";
+    String wrongQuestionTwo = "Two is the correct answer";
+    String wrongQuestionThree = "Three is the correct answer";
+
+    //initializing score
     int score = 0;
 
     @Override
@@ -30,30 +38,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
+    //when submit is clicked
+    public void submitScore(View view) {
 
-        CheckBox oneLolliPop = (CheckBox) findViewById(R.id.whippedCream);
-        CheckBox twoLolliPop = (CheckBox) findViewById(R.id.chocolate);
-        //Log.v("MainActivity", cName );
+        //boolean values for check boxes question 1, could this be moved into it's own function
+        CheckBox oneCheckQuestionOne = (CheckBox) findViewById(R.id.check_1_question_1);
+        CheckBox twoCheckQuestionTwo = (CheckBox) findViewById(R.id.check_1_question_2);
+        CheckBox threeCheckQuestionTwo = (CheckBox) findViewById(R.id.check_1_question_3);
+        CheckBox fourCheckQuestionTwo = (CheckBox) findViewById(R.id.check_1_question_4);
 
-        boolean hasOneLolliPop = oneLolliPop.isChecked();
-        boolean hasTwoLolliPop = twoLolliPop.isChecked();
+        boolean hasCheckQuestionOne = oneCheckQuestionOne.isChecked();
+        boolean hasCheckQuestionTwo = twoCheckQuestionTwo.isChecked();
+        boolean hasCheckQuestionThree = threeCheckQuestionTwo.isChecked();
+        boolean hasCheckQuestionFour = fourCheckQuestionTwo.isChecked();
 
-        EditText customerNameText   = (EditText)findViewById(R.id.name);
-        String customerName = customerNameText.getText().toString();
+        //get answer for fill question one and convert it to uppercase
+        EditText answerFillQuestionOne = (EditText) findViewById(R.id.name);
+        String fillQuestionOne = answerFillQuestionOne.getText().toString();
 
-        customerName = customerName.toUpperCase();
+        fillQuestionOne = fillQuestionOne.toUpperCase();
 
+        //Calculating radio questions portion of score
         score = question1 + question2 + question3;
-        if (hasOneLolliPop && hasTwoLolliPop) {
-            score++;
-        }
 
 
-        // Set textview
+        // used so we can add the answer to html could these be in there own function??
         TextView questionOneTextView = (TextView) findViewById(R.id.answer_radio_question_1);
         TextView questionTwoTextView = (TextView) findViewById(R.id.answer_radio_question_2);
         TextView questionThreeTextView = (TextView) findViewById(R.id.answer_radio_question_3);
@@ -61,134 +70,132 @@ public class MainActivity extends AppCompatActivity {
         TextView questionOneFillTextView = (TextView) findViewById(R.id.answer_fill_question_1);
 
 
-        if (customerName.equals("CAT")) {
-            questionOneFillTextView.setText("correct");
+        //lets user know if they were right or wrong
+        displayMessageCheckBox(hasCheckQuestionOne, hasCheckQuestionTwo, hasCheckQuestionThree, hasCheckQuestionFour, questionOneCheckTextView);
+        displayMessageRadioQuestion(question1, questionOneTextView, wrongQuestionOne);
+        displayMessageRadioQuestion(question2, questionTwoTextView, wrongQuestionTwo);
+        displayMessageRadioQuestion(question3, questionThreeTextView, wrongQuestionThree);
+
+        //checking to see if fill question one is correct should probably be moved to it's own function
+        if (fillQuestionOne.equals("CAT")) {
+            questionOneFillTextView.setText("Correct");
             score++;
+        } else {
+            questionOneFillTextView.setText("Wrong, it's spelled splat");
         }
-        else {
-            questionOneFillTextView.setText("wrong");
-        }
 
 
-        displayMessageCheckBox(hasOneLolliPop, hasTwoLolliPop, questionOneCheckTextView);
-        displayMessageRadioQuestion2(question1, questionOneTextView);
-        displayMessageRadioQuestion2(question2, questionTwoTextView);
-        displayMessageRadioQuestion2(question3, questionThreeTextView);
-
-        String message = createOrderSummary(hasOneLolliPop, hasTwoLolliPop, score);
+        String message = finalScoreSummary(score);
         displayMessage(message);
 
     }
 
+    //creates meessage for final score
+    private String finalScoreSummary(int score) {
+        String priceMessage = "Your final score is: " + score;
 
+        return priceMessage;
+    }
+
+    // displays final score message
+    private void displayMessage(String message) {
+        TextView finalScoreTextView = (TextView) findViewById(R.id.final_score_text_view);
+        finalScoreTextView.setText(message);
+    }
+
+    //used for radio questions need to add a string message showing right answer
+    private void displayMessageRadioQuestion(int isCorrect, TextView answer, String wrongAnswer) {
+
+        if (isCorrect == 1) {
+            answer.setText("Correct");
+        } else {
+            answer.setText(wrongAnswer);
+        }
+    }
+
+    //used for check box question, may need a different one for each need to add a string message showing right answer
+    private void displayMessageCheckBox(Boolean A, Boolean B, Boolean C, Boolean D, TextView answer) {
+
+        if (A && B && !C && !D) {
+            answer.setText("Correct");
+            score++;
+        } else {
+            answer.setText("Wrong, 1 & 2 Sir/Ma'am/gram");
+        }
+    }
+
+
+    //checks all the radio questions
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((android.widget.RadioButton) view).isChecked();
 
-        // Check which radio button was clicked
+        // Check which radio button was clicked for radio question 1
         switch (view.getId()) {
             case R.id.radio_one:
                 if (checked)
-                    question1 = 1;// Pirates are the best
+                    question1 = 1;// Correct
                 break;
             case R.id.radio_two:
                 if (checked)
-                    question1 = 0;// Ninjas rule
+                    question1 = 0;// wrong
             {
                 break;
             }
             case R.id.radio_three:
                 if (checked)
-                    question1 = 0;// Pirates are the best
+                    question1 = 0;// wrong
                 break;
             case R.id.radio_four:
                 if (checked)
-                    question1 = 0;// Ninjas rule
+                    question1 = 0;// wrong
             {
                 break;
             }
         }
 
+        // Check which radio button was clicked for radio question 2
         switch (view.getId()) {
             case R.id.radio2_one:
                 if (checked)
-                    question2 = 0;// Pirates are the best
+                    question2 = 0;// wrong
                 break;
             case R.id.radio2_two:
                 if (checked)
-                    question2 = 1;// Ninjas rule
+                    question2 = 1;// right
                 break;
             case R.id.radio2_three:
                 if (checked)
-                    question2 = 0;// Pirates are the best
+                    question2 = 0;// wrong
                 break;
             case R.id.radio2_four:
                 if (checked)
-                    question2 = 0;// Ninjas rule
+                    question2 = 0;// wrong
             {
                 break;
             }
         }
 
+        // Check which radio button was clicked for radio question 3
         switch (view.getId()) {
             case R.id.radio3_one:
                 if (checked)
-                    question3 = 0;// Pirates are the best
+                    question3 = 0; // wrong
                 break;
             case R.id.radio3_two:
                 if (checked)
-                    question3 = 0;// Ninjas rule
+                    question3 = 0; // wrong
                 break;
             case R.id.radio3_three:
                 if (checked)
-                    question3 = 1;// Pirates are the best
+                    question3 = 1; // right
                 break;
             case R.id.radio3_four:
                 if (checked)
-                    question3 = 0;// Ninjas rule
+                    question3 = 0; // wrong
             {
                 break;
             }
-        }
-    }
-
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private String createOrderSummary(boolean addWhippedCream, boolean addChocolate, int score) {
-        String priceMessage = "" + addWhippedCream;
-        priceMessage += "\n" + addChocolate;
-        priceMessage += "\n" + score;
-
-        return priceMessage;
-    }
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.answer_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
-
-    private void displayMessageRadioQuestion2(int isCorrect, TextView answer) {
-
-        if (isCorrect == 1) {
-        answer.setText("correct");
-        }
-        else {
-            answer.setText("wrong");
-        }
-    }
-
-    private void displayMessageCheckBox(Boolean A, Boolean B, TextView answer) {
-
-        if (A && B) {
-            answer.setText("correct");
-        }
-        else {
-            answer.setText("wrong");
         }
     }
 
